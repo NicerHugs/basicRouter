b55eb796d7530de71dfaa4f2fd148f45ba748659
Jess Scheuring <jscheuring@gmail.com>
2015-09-23 16:50:47 -0400

    cleans server

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,6 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 
-gulp.task('hi', function() {
-  console.log('hi');
-});
-
 gulp.task('serve', function() {
   connect.server({
     port: 9000
